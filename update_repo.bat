242b6ba72b0a4f913b0b16dfcffb56881d5fe3e5
Rose, Maxime <maxime.rose@cgi.com>
2015-10-15 16:02:32 +0200

    adding script

---
+++ update_repo.bat
@@ -0,0 +1,31 @@
+@echo off
+
+cd C:\Users\rosemx\Documents\homework\homework
+
+git log | head -n 5
+
+choice /C YN /M "New commit ?"
+
+IF errorlevel 2 goto exit
+IF errorlevel 1 goto commit
+
+
+:commit
+SET commitMessage=
+SET /p commitMessage= Type a message for the new commit : 
+::echo message : %commitMessage%
+git commit -a -m "%commitMessage%"
+SLEEP 1
+
+choice /C YN /M "Push commit ?"
+
+IF errorlevel 2 goto exit
+IF errorlevel 1 goto push
+
+:push
+git push origin master
+goto exit
+
+:exit
+SLEEP 5
+exit
